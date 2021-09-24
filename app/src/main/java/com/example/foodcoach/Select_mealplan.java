3af9f795610e2e0a71055af99052deea49bd470e
maleeshac <89582165+maleeshac@users.noreply.github.com>
2021-09-24 13:13:56 +0530

    Add Maleesha UI changes

---
+++ app/src/main/java/com/example/foodcoach/Select_mealplan.java
@@ -0,0 +1,14 @@
+package com.example.foodcoach;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Select_mealplan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_mealplan);
+    }
+}
