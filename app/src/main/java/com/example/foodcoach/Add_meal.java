deb25f359b5d082ea7479cc42fda174ee0f282e7
maleeshac <89582165+maleeshac@users.noreply.github.com>
2021-09-25 22:03:21 +0530

    Maleesha CRUD operations

--- app/src/main/java/com/example/foodcoach/Add_meal.java
+++ app/src/main/java/com/example/foodcoach/Add_meal.java
@@ -1,14 +1,43 @@
 package com.example.foodcoach;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Add_meal extends AppCompatActivity {
 
+    EditText edtBrmeal,edtBrtime,edtLumeal,edtLutime,edtDimeal,edtDitime;
+    Button btn_addmeal;
+
+    public static SQLiteHelper sqLiteHelper;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meal);
+
+        init();
+
+        sqLiteHelper=new SQLiteHelper(this,"ADDMEALDB.sqlite",null,1);
+
+        sqLiteHelper.queryData("CREATE TABLE IF NOT EXISTS FOOD(Id INTEGER PRIMARY KEY AUTOINCREMENT,breakfast VARCHAR, brmeal VARCHAR, brtime VARCHAR, lunch VARCHAR, lumeal VARCHAR, lutime VARCHAR, dinner VARCHAR, dimeal VARCHAR, ditime VARCHAR)");
     }
+    private  void init(){
+
+        edtBrmeal=(EditText) findViewById(R.id.edtBrmeal);
+        edtBrtime=(EditText) findViewById(R.id.edtBrtime);
+
+        edtLumeal=(EditText) findViewById(R.id.edtLumeal);
+        edtLutime=(EditText) findViewById(R.id.edtLutime);
+
+        edtDimeal=(EditText) findViewById(R.id.edtDimeal);
+        edtDitime=(EditText) findViewById(R.id.edtDitime);
+        btn_addmeal=(Button) findViewById(R.id.btn_addmeal);
+    }
+
+
 }
--- app/src/main/java/com/example/foodcoach/Select_age.java
+++ app/src/main/java/com/example/foodcoach/Select_age.java
@@ -1,6 +1,9 @@
 package com.example.foodcoach;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,35 @@ public class Select_age extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_age);
+
+
+        Button button1 = (Button) findViewById(R.id.btn_1to5);
+        Button button2 = (Button) findViewById(R.id.btn_6to17);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                openAdd_meal();
+            }
+
+            private void openAdd_meal() {
+                Intent intent1=new Intent(Select_age.this, Add_meal.class);
+                startActivity(intent1);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAdd_meal();
+            }
+
+            private void openAdd_meal() {
+                Intent intent2=new Intent(Select_age.this, Add_meal.class);
+                startActivity(intent2);
+            }
+        });
+
+
     }
 }
